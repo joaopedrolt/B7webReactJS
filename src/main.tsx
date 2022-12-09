b5023c15815f8482577dbb9b66ca2ecca9f154d9
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2022-12-08 21:56:36 -0300

    Reducer fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import { ContextProvider } from './contexts/ContextReducer';
+import { ContextProvider } from './contexts/Context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
